2d5020e42b77464b87ba8e1ac17275d6e5981258
dhruvahujadda <126565175+dhruvahujadda@users.noreply.github.com>
2023-08-21 02:02:55 +0530

    SQLQuery.sql

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,25 +1,25 @@
-drop database Hospital_Management_System;
+DROP DATABASE hospital_management_system;
 
-create database Hospital_Management_system;
+DROP TABLE donor;
+DROP TABLE patient;
+DROP TABLE doctors;
 
-drop table donor;
-drop table patient;
-drop table doctors;
+CREATE DATABASE hospital_management_system;
 
-create table donor(
-donor_id varchar(5) primary key,
-donorname varchar(20),
-Contact int,
-Age int,
-Any_disease varchar(50),
-Blood_group varchar(3),
-Haemoglobin int,
-unit_blood int)
-;
+CREATE TABLE donor (
+    donor_id VARCHAR(5) PRIMARY KEY,
+    donorname VARCHAR(20),
+    Contact INT,
+    Age INT,
+    Any_disease VARCHAR(50),
+    Blood_group VARCHAR(3),
+    Haemoglobin INT,
+    unit_blood INT
+);
 
 
-insert into donor(donor_id,donorname,Contact,Age,Any_disease,Blood_group,Haemoglobin,unit_blood) 
-values('D1','Dhruv Ahuja',0000000000,21,'No','A+',12,1.5),
+INSERT INTO donor(donor_id,donorname,Contact,Age,Any_disease,Blood_group,Haemoglobin,unit_blood) 
+VALUES('D1','Dhruv Ahuja',0000000000,21,'No','A+',12,1.5),
 ('D2','Arun Verma',0000000000,20,'No','O+',11,1),
 ('D3','Ramesh Kumar',0000000000,27,'No','B+',11.5,1),
 ('D4','Girish Sharma',0000000000,32,'Asthma','B-',11,1.5),
@@ -32,17 +32,17 @@ values('D1','Dhruv Ahuja',0000000000,21,'No','A+',12,1.5),
 ('D11','Vartika Madan',0000000000,26,'No','AB-',12.5,1.5),
 ('D12','Krishna Chawla',0000000000,31,'Diabetes','AB+',14,1);
 
-create table patient(
-patient_id varchar(5) primary key,
-patient_name varchar(20),
-Contact int,
-Age int,
-Disease varchar(50) ,
-Blood_group varchar(3)
+CREATE TABLE patient (
+    patient_id VARCHAR(5) PRIMARY KEY,
+    patient_name VARCHAR(20),
+    Contact INT,
+    Age INT,
+    Disease VARCHAR(50),
+    Blood_group VARCHAR(3)
 );
 
-insert into patient(patient_id,patient_name,Contact,Age,Disease,Blood_group) 
-values ('P1','Ravi Kapoor', 1111111111,71,'Anemia','AB+'),
+INSERT INTO patient(patient_id,patient_name,Contact,Age,Disease,Blood_group) 
+VALUES ('P1','Ravi Kapoor', 1111111111,71,'Anemia','AB+'),
 ('P2','Sonu Pathak',1111111111,12,'Thalassemia','O+'),
 ('P3','Rishi Chopra', 1111111111,28,'Accident','B+'),
 ('P4','Ishan Dudeja',1111111111,26,'Accident','A+'),
@@ -52,13 +52,44 @@ values ('P1','Ravi Kapoor', 1111111111,71,'Anemia','AB+'),
 ('P8','Robin Singh',1111111111,12,'Thalassemia','O+'),
 ('P9','Aryan Saroj', 1111111111,22,'Accident','A+');
 
-create table doctors(
-Doctor_Id varchar(5) primary key,
-Doctor_name varchar(20),
-doctor_type varchar(20),
+CREATE TABLE doctors (
+    Doctor_Id VARCHAR(5) PRIMARY KEY,
+    Doctor_name VARCHAR(20),
+    doctor_type VARCHAR(20)
 );
 
-insert into doctors(Doctor_Id,Doctor_name,doctor_type) 
-values ('Dr1','Arjun Reddy','Anemia'),
+INSERT INTO doctors(Doctor_Id,Doctor_name,doctor_type) 
+VALUES ('Dr1','Arjun Reddy','Anemia'),
 ('Dr2','Kabir Singh','Thalassemia'),
 ('Dr3','Gaurav Mohanty','Accident');
+
+CREATE TABLE donation (
+    Donation_Id VARCHAR(5) PRIMARY KEY,
+    Donor_Id VARCHAR(5),
+    Donation_Date DATE,
+    FOREIGN KEY (Donor_Id)
+        REFERENCES donor (donor_id)
+);
+
+INSERT INTO donation(Donation_Id, Donor_Id, Donation_Date)
+VALUES
+    ('DN1', 'D1', '2023-08-15'),
+    ('DN2', 'D3', '2023-08-18'),
+    ('DN3', 'D6', '2023-08-20');
+
+CREATE TABLE treatment (
+    Treatment_Id VARCHAR(5) PRIMARY KEY,
+    Patient_Id VARCHAR(5),
+    Doctor_Id VARCHAR(5),
+    Treatment_Date DATE,
+    FOREIGN KEY (Patient_Id)
+        REFERENCES patient (patient_id),
+    FOREIGN KEY (Doctor_Id)
+        REFERENCES doctors (Doctor_Id)
+);
+
+INSERT INTO treatment(Treatment_Id, Patient_Id, Doctor_Id, Treatment_Date)
+VALUES
+    ('T1', 'P2', 'Dr2', '2023-08-15'),
+    ('T2', 'P4', 'Dr3', '2023-08-18'),
+    ('T3', 'P6', 'Dr2', '2023-08-20');
